2c1c3e203a402e3007e42ff6260d437e34645898
Sakhi <sakhiasokan@gmail.com>
2021-06-10 22:11:17 +0530

    adding js funtamentals in 10 th june

---
+++ javascriptprograms/fundamental.js
@@ -0,0 +1,16 @@
+console.log("hello js")
+var age=29;
+var p_name="sakhi";
+console.log("hai i am",p_name,age,"year old")
+
+var state="kerala";
+var active_cases=15000;
+var death=200;
+var cured_cases=1000;
+
+console.log("in",state,active_cases,"new cares and",death,"death has been reported");
+//node console.log(`in ${state} ${active_cases} and ${cured_cases} death ${death} has been registered`);
+
+
+
+
